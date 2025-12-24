53f6bad3ece79c3a82d5d632f9a0e33a3456b1a9
tagovertag <tagovertag@gmail.com>
2025-12-24 11:56:50 -0300

    chore: add shadcn, prettier and eslint setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ["http://192.168.3.35:3000", "http://localhost:3000"],
+  },
 };
-
-export default nextConfig;
+module.exports = nextConfig;
